5fc074b031f7fff1aa09adcaffbb3abe8af91043
Clay Levering <claylevering@gmail.com>
2016-10-07 10:29:10 -0700

    Fixing paste issue

--- packages/react-scripts/config/customizers.js
+++ packages/react-scripts/config/customizers.js
@@ -65,15 +65,4 @@ module.exports = {
     }
   }
 }
-t('style', 'css!postcss!stylus')
-            }
-        }
-    },
-    'CSS_MODULES': {
-        config: {
-            dev: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
-            prod: 'style!css?modules&-autoprefixer&importLoaders=1!postcss'
-        }
-    }
-}
 
