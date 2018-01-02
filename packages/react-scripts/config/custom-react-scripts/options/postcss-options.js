8679b131dac1cdfd0d7e6e57d08f56b9b089b48b
Muthiah, Ramachandran <ramachandran.muthiah@pearson.com>
2018-01-02 10:23:42 +0530

    Include post css constants in sass loader

--- packages/react-scripts/config/custom-react-scripts/options/postcss-options.js
+++ packages/react-scripts/config/custom-react-scripts/options/postcss-options.js
@@ -4,6 +4,8 @@ module.exports = {
   ident: 'postcss',
   plugins: () => [
     require('postcss-flexbugs-fixes'),
+    require('precss'),
+    require('postcss-constants'),
     autoprefixer({
       browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
       flexbox: 'no-2009',
